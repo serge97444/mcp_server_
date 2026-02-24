c8fa6b5906030fd5c7fc9e2fecb915e403a005d8
serge97444 <benyamin.serge@gmail.com>
2026-02-24 19:33:44 +0100

    Add files via upload

--- apps/rich-ui/src/main.jsx
+++ apps/rich-ui/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 
+// same logic as in readhost but happens once at start
 const host = window.openai || {};
 const toolOutput = host.toolOutput;
 const data =
@@ -13,7 +14,8 @@ const data =
   host.widgetState ||
   window.__APP_DATA__ ||
   {};
-
+// function allowing to read the parameters of a request
+console.log("window location " , window.location.search);
 const params = new URLSearchParams(window.location.search);
 const debug =
   params.get("debug") === "1" ||
